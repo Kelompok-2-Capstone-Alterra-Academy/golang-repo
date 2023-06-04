60de59a732f98740c2c9dfd4bcb701df1c57ba1c
mrefawaladam <aerfa55@gmail.com>
2023-06-04 10:40:19 +0700

    create entity modules

---
+++ internal/entity/Module.go
@@ -0,0 +1,13 @@
+package entity
+
+import "gorm.io/gorm"
+
+type Module struct {
+	*gorm.Model
+
+	Name        string `json:"name" form:"name" validate:"required"`
+	Email       string `json:"email" form:"email" validate:"required,email" `
+	Password    string `json:"password" form:"password" validate:"required"`
+	Role        string `gorm:"default:students"`
+	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required"`
+}
