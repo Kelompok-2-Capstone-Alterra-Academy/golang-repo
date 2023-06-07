14575765a8a154767d526d6946c0ef525777171b
yoviadhi123@gmail.com <yoviadhi123@gmail.com>
2023-06-07 19:15:32 +0700

    fix attachment usecase

--- internal/application/usecase/attachment_usecase.go
+++ internal/application/usecase/attachment_usecase.go
@@ -9,11 +9,31 @@ type AttachmentUseCase struct {
 	Repo repository.AttachmentRepository
 }
 
-func (usecase AttachmentUseCase) GetAttachmentByCourseSectionIDAndAttachmentID(courseSectionID string, attachmentID string) (*entity.Attachment, error) {
-	attachment, err := usecase.Repo.GetAttachmentByCourseSectionIDAndAttachmentID(courseSectionID, attachmentID)
-	if err != nil {
-		return nil, err
-	}
+func (usecase AttachmentUseCase) GetAllAttachments(folderId int) ([]entity.Attachment, error) {
+	Attachmentes, err := usecase.Repo.GetAllAttachments(folderId)
+	return Attachmentes, err
+}
 
-	return attachment, nil
+func (usecase AttachmentUseCase) GetAttachment(id int) (entity.Attachment, error) {
+	Attachment, err := usecase.Repo.GetAttachment(id)
+	return Attachment, err
+}
+
+func (usecase AttachmentUseCase) CreateAttachment(Attachment entity.Attachment) error {
+	err := usecase.Repo.CreateAttachment(Attachment)
+	return err
+}
+
+func (usecase AttachmentUseCase) UpdateAttachment(id int, Attachment entity.Attachment) error {
+	err := usecase.Repo.UpdateAttachment(id, Attachment)
+	return err
+}
+
+func (usecase AttachmentUseCase) DeleteAttachment(id int) error {
+	err := usecase.Repo.DeleteAttachment(id)
+	return err
+}
+func (usecase AttachmentUseCase) FindAttachment(id int) error {
+	err := usecase.Repo.FindAttachment(id)
+	return err
 }
