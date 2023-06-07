0c86345190690fd6d2e45294689f351acc875b8b
yoviadhi123@gmail.com <yoviadhi123@gmail.com>
2023-06-07 19:15:23 +0700

    fix attachment repository

--- internal/adapters/repository/attachment_repository.go
+++ internal/adapters/repository/attachment_repository.go
@@ -10,14 +10,37 @@ type AttachmentRepository struct {
 	DB *gorm.DB
 }
 
-func (repo AttachmentRepository) GetAttachmentByCourseSectionIDAndAttachmentID(courseSectionID string, attachmentID string) (*entity.Attachment, error) {
-	attachment := &entity.Attachment{}
-	result := repo.DB.Preload("Mentor").
-		Where("id = ? AND course_section_id = ?", attachmentID, courseSectionID).
-		First(attachment)
+func (repo AttachmentRepository) GetAllAttachments(folder int) ([]entity.Attachment, error) {
+	var Attachments []entity.Attachment
+	result := repo.DB.Where("folder_id = ?", folder).Find(&Attachments)
 	if result.Error != nil {
 		return nil, result.Error
 	}
+	return Attachments, nil
+}
 
-	return attachment, nil
+func (repo AttachmentRepository) GetAttachment(id int) (entity.Attachment, error) {
+	var Attachments entity.Attachment
+	result := repo.DB.First(&Attachments, id)
+	return Attachments, result.Error
+}
+
+func (repo AttachmentRepository) CreateAttachment(Attachment entity.Attachment) error {
+	result := repo.DB.Create(&Attachment)
+	return result.Error
+}
+
+func (repo AttachmentRepository) UpdateAttachment(id int, Attachment entity.Attachment) error {
+	result := repo.DB.Model(&Attachment).Where("id = ?", id).Updates(&Attachment)
+	return result.Error
+}
+
+func (repo AttachmentRepository) DeleteAttachment(id int) error {
+	result := repo.DB.Delete(&entity.Attachment{}, id)
+	return result.Error
+}
+
+func (repo AttachmentRepository) FindAttachment(id int) error {
+	result := repo.DB.First(&entity.Attachment{}, id)
+	return result.Error
 }
