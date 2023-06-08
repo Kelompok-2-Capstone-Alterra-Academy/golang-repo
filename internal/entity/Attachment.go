530438ed6197fbf0013f281a9593ec98cdef0a4a
mrefawaladam <aerfa55@gmail.com>
2023-06-08 21:41:54 +0700

    fixing entity

--- internal/entity/Attachment.go
+++ internal/entity/Attachment.go
@@ -9,7 +9,7 @@ type Attachment struct {
 	Description      string `json:"description" form:"description" validate:"required"`
 	Type             string `json:"type" form:"type" validate:"required"`
 	AttachmentSource string `json:"attachment_source" form:"attachment_source" validate:"required"`
-	Status           string `json:"status" gorm:"default:students"`
+	Status           string `json:"status" gorm:"default:draft"`
 	FolderId         string `json:"folder_id" form:"folder_id"`
 	Folder           Folder `json:"folder" gorm:"foreignKey:FolderId"`
 }
