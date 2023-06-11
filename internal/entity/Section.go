a84a3263a4f688a29402101b58a1cad1ad0bf201
yoviadhi123@gmail.com <yoviadhi123@gmail.com>
2023-06-11 23:17:45 +0700

    fix Section.go

--- internal/entity/Section.go
+++ internal/entity/Section.go
@@ -1,10 +1,12 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Section struct {
 	*gorm.Model
+	CourseId   int    `json:"course_id" form:"course_id"`
+	Course     Course `gorm:"foreignKey:CourseId"`
 	CourseName string `json:"course_name" form:"course_name"`
-	CourseId   string `json:"course_id" form:"course_id"`
-	Course     Course `json:"course" gorm:"foreignKey:CourseId"`
 }
