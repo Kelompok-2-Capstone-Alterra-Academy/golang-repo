efedbb33ed25f045ecd870823d7e1d34bb20bd82
mrefawaladam <aerfa55@gmail.com>
2023-06-12 16:23:52 +0700

    fix main

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	r "capston-lms/internal/adapters/http"
+	"capston-lms/internal/adapters/http"
+	"fmt"
+	"log"
+
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	e := r.InitRoutes()
+	e := http.InitRoutes()
 	e.Debug = true
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Pre(middleware.HTTPSRedirect())
+
+	address := fmt.Sprintf(":%s", viper.GetString("EXSPOSE_PORT"))
+	if address == ":" {
+		address = ":8080" // Port default 8080 jika PORT tidak diset
+	}
+	// Load certificate and key from file
+	certFile := "cert.pem"
+	keyFile := "key.pem"
+	err := e.StartTLS(address, certFile, keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
