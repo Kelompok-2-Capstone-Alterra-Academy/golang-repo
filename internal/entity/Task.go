f41edbb4499c84c2e869056df17cc2c193a32205
mrefawaladam <aerfa55@gmail.com>
2023-06-12 21:52:29 +0700

    fixing task

--- internal/entity/Task.go
+++ internal/entity/Task.go
@@ -1,14 +1,12 @@
 package entity
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Task struct {
 	*gorm.Model
-	DueDate  time.Time `json:"due_date" form:"due_date"`
-	ModuleId string    `json:"module_id" form:"module_id"`
-	Module   *Module   `json:"module,omitempty" gorm:"foreignKey:ModuleId"`
+	DueDate  string  `json:"due_date" form:"due_date"`
+	ModuleId int     `json:"module_id" form:"module_id"`
+	Module   *Module `json:"module,omitempty" gorm:"foreignKey:ModuleId"`
 }
