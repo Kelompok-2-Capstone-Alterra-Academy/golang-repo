dfc297dc9ae3fda9ce62b4ff776b8aac69caf2de
mrefawaladam <aerefa55@gmail.com>
2023-06-16 22:01:15 +0700

    fix description

--- internal/entity/promo.go
+++ internal/entity/promo.go
@@ -10,6 +10,7 @@ type Promo struct {
 	*gorm.Model
 
 	PromoName   string    `json:"promo_name" form:"promo_name"`
+	Description string    `json:"description" form:"description"`
 	ExpiredDate time.Time `json:"expired_date" form:"expired_date"`
 	TotalPromo  float64   `json:"total_promo" form:"total_promo"`
 	Thumbnail   string    `json:"thumbnail" form:"thumbnail" validate:"required"`
