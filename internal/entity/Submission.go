3b2d30bd54f6f3887454739079ea48a68534a832
mrefawaladam <aerfa55@gmail.com>
2023-06-18 23:03:15 +0700

    fix entity

--- internal/entity/Submission.go
+++ internal/entity/Submission.go
@@ -10,6 +10,6 @@ type Submission struct {
 	Type             string  `json:"type" form:"type"`
 	Notes            string  `json:"notes" form:"notes"`
 	Module           *Module `json:"module,omitempty" gorm:"foreignKey:ModuleId"`
-	StudentId        string  `json:"student_id" form:"student_id"`
+	StudentId        int     `json:"student_id" form:"student_id"`
 	User             *User   `json:"student,omitempty" gorm:"foreignKey:StudentId"`
 }
