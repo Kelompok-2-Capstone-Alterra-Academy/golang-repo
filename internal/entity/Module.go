55551edc75e5de2e164a8d075c513f3fe9d8377f
mrefawaladam <aerfa55@gmail.com>
2023-06-20 23:49:41 +0700

    fix entity module

--- internal/entity/Module.go
+++ internal/entity/Module.go
@@ -1,6 +1,32 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"database/sql"
+	"encoding/json"
+
+	"gorm.io/gorm"
+)
+
+type NullInt struct {
+	sql.NullInt64
+}
+
+func (ni *NullInt) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		ni.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(data, &ni.Int64)
+	ni.Valid = (err == nil)
+	return err
+}
+
+func (ni NullInt) MarshalJSON() ([]byte, error) {
+	if !ni.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(ni.Int64)
+}
 
 type Module struct {
 	*gorm.Model
@@ -8,7 +34,7 @@ type Module struct {
 	Description  string       `json:"description" form:"description"`
 	SectionId    int          `json:"section_id" form:"section_id"`
 	Section      Section      `json:"section" gorm:"foreignKey:SectionId"`
-	AttachmentId *int         `json:"attachment_id" form:"attachment_id"`
+	AttachmentId NullInt      `json:"attachment_id" form:"attachment_id"`
 	Attachment   Attachment   `json:"attachment,omitempty" gorm:"foreignKey:AttachmentId"`
 	Tasks        []Task       `json:"tasks" gorm:"foreignKey:ModuleId"`
 	Submission   []Submission `json:"submission" gorm:"foreignKey:ModuleId"`
