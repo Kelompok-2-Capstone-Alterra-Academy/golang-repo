525c9bc615ae265454e61e21541a4eed56a80877
mrefawaladam <aerefa55@gmail.com>
2023-06-26 12:46:43 +0700

    fix payment

--- internal/application/usecase/transaction_usecase.go
+++ internal/application/usecase/transaction_usecase.go
@@ -4,6 +4,7 @@ import (
 	"capston-lms/internal/adapters/repository"
 	"capston-lms/internal/entity"
 	"fmt"
+	"log"
 
 	"github.com/midtrans/midtrans-go"
 	"github.com/midtrans/midtrans-go/snap"
@@ -79,6 +80,9 @@ func (uc *TransactionUsecase) GenerateSnapReq(TransactionID uint, UserID int, To
 	// Create ItemDetails array for Snap Request
 	var itemDetails []midtrans.ItemDetails
 	var totalPrice int64 = 0
+	coursePrice := 0
+	totalPayment := 0
+
 	for _, bo := range courseOrders {
 		itemDetails = append(itemDetails, midtrans.ItemDetails{
 			ID:    bo.CourseId,
@@ -86,9 +90,33 @@ func (uc *TransactionUsecase) GenerateSnapReq(TransactionID uint, UserID int, To
 			Qty:   int32(1),
 			Name:  bo.Course.CourseName,
 		})
+		coursePrice = bo.Price
+		totalPayment = bo.TotalPayment
 		totalPrice += int64(bo.Price) * int64(1)
 	}
+	totalPrice += 500
+	itemDetails = append(itemDetails, midtrans.ItemDetails{
+		ID:    "5000",
+		Price: int64(500),
+		Qty:   int32(1),
+		Name:  "Admin Fee",
+	})
+	log.Println("Ini adalah log level Info", totalPrice)
+	log.Println("Ini adalah log level payment", totalPayment)
+	log.Println("Ini adalah log level price", coursePrice)
 
+	if totalPrice < int64(totalPayment) {
+		totalPromo := (int64(totalPayment) - totalPrice)
+		log.Println("Ini adalah log level promo", totalPromo)
+
+		itemDetails = append(itemDetails, midtrans.ItemDetails{
+			ID:    "1000",
+			Price: int64(totalPromo),
+			Qty:   int32(1),
+			Name:  "Promo",
+		})
+		totalPrice += totalPromo
+	}
 	// Create Snap Request object
 	snapReq := &snap.Request{
 		TransactionDetails: midtrans.TransactionDetails{
